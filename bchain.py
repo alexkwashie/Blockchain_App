d9a1aa02560072d5daebaaa57243d90288dc329e
Alex A. Kwashie <alex.kwashie@hotmail.com>
2018-10-28 07:34:14 +0000

    for & while loops

--- bchain.py
+++ bchain.py
@@ -1,26 +1,39 @@
 blockchain = []
 
-def get_last_amount():
-    return blockchain[-1]
-# '-1' used to select last element in an array
 
-def add_val(val, last_num = 1):
-    blockchain.append([last_num, val]) #2 !important - last_num is set to defult [1]
+def get_last_amount():
+    """Returns last value in blockchain array"""
+    return blockchain[-1]
+
+
+def add_val(val, last_num=1):
+    """Append a new value as well the last value in the blockchain
+
+    Arguments:
+        :val : Is the value that must be manually added
+        : last_num: is the last amount in the blockchain  (defults to '1') to get it started
+    """
+    blockchain.append(
+        [last_num, val])  # 2 !important - last_num is set to defult [1]
+
 
 def get_user_input():
-    return  input('What is your transaction amount: ')
+    return input('What is your transaction amount: ')
 
 
 tx_amount = get_user_input()
 add_val(tx_amount)
-
 #add_val(get_last_amount(), 9.2)
 
-tx_amount = get_user_input()
-add_val(last_num=get_last_amount(), val = tx_amount)
 
-tx_amount = get_user_input()
-add_val(tx_amount, get_last_amount())
+#Creat a while loo to ask for user input
+while True:
+    tx_amount = get_user_input()
+    add_val(tx_amount, get_last_amount())
 
+    #Outputting the list in the blockchain
+    for block in blockchain:
+        print('Outputting block')
+        print(block)
 
-print(blockchain)
+print('Done!')
