5e8f1cef7678a8f6336693ff63306be3344458b6
Alex A. Kwashie <alex.kwashie@hotmail.com>
2018-10-28 08:54:45 +0000

    resolving bug: remeber to put '()' at the end of every function call

--- bchain.py
+++ bchain.py
@@ -1,7 +1,7 @@
 blockchain = []
 
 
-def get_last_amount():
+def get_blockchain_amount():
     """Returns last value in blockchain array"""
     return blockchain[-1]
 
@@ -17,23 +17,33 @@ def add_val(val, last_num=1):
         [last_num, val])  # 2 !important - last_num is set to defult [1]
 
 
-def get_user_input():
+def get_transaction_val():
     return input('What is your transaction amount: ')
 
+def get_user_choice():
+    user_input = input('Your Choice: ')
+    return user_input
 
-tx_amount = get_user_input()
-add_val(tx_amount)
-#add_val(get_last_amount(), 9.2)
-
-
-#Creat a while loo to ask for user input
-while True:
-    tx_amount = get_user_input()
-    add_val(tx_amount, get_last_amount())
-
+def print_blockchain_element():
     #Outputting the list in the blockchain
     for block in blockchain:
         print('Outputting block')
         print(block)
 
+tx_amount = get_transaction_val()
+add_val(tx_amount)
+
+#Creat a while loo to ask for user input
+while True:
+    print('Please choose option')
+    print('1: Add transaction value')
+    print('2: Output Block chain blocks')
+    #put get_user_choice() input in  a variable
+    user_choice = get_user_choice()
+    if user_choice == 1:
+        tx_amount = get_transaction_val()
+        add_val(tx_amount, get_blockchain_amount())
+    else:
+        print_blockchain_element()
+    break
 print('Done!')
