b02269361e3bf8f38b6ac1a123fbe554b886370b
Alex A. Kwashie <alex.kwashie@hotmail.com>
2018-10-29 14:16:32 +0000

    using 'None' and reviewing the code

--- bchain.py
+++ bchain.py
@@ -1,20 +1,23 @@
-blockchain = []
 
+blockchain = []
 
 def get_blockchain_amount():
     """Returns last value in blockchain array"""
+    if len(blockchain) <  1:
+        return None
     return blockchain[-1]
 
 
-def add_val(val, last_num=1):
+def add_val(val, last_num):
     """Append a new value as well the last value in the blockchain
 
     Arguments:
         :val : Is the value that must be manually added
         : last_num: is the last amount in the blockchain  (defults to '1') to get it started
     """
-    blockchain.append(
-        [last_num, val])  # 2 !important - last_num is set to defult [1]
+    if last_num == None:
+        last_num = [1]
+    blockchain.append([last_num, val])  # 2 !important - last_num is set to defult [1]
 
 
 def get_transaction_val():
@@ -30,20 +33,28 @@ def print_blockchain_element():
         print('Outputting block')
         print(block)
 
-tx_amount = get_transaction_val()
-add_val(tx_amount)
 
 #Creat a while loo to ask for user input
 while True:
     print('Please choose option')
     print('1: Add transaction value')
     print('2: Output Block chain blocks')
+    print('q: Quit')
+
     #put get_user_choice() input in  a variable
     user_choice = get_user_choice()
-    if user_choice == 1:
+    if user_choice == '1':
         tx_amount = get_transaction_val()
         add_val(tx_amount, get_blockchain_amount())
-    else:
+    elif user_choice == '2' and len(blockchain) < 1:
+        print('Blockchain is empty, enter valid amount')
+    elif user_choice == '2':
         print_blockchain_element()
-    break
+    elif user_choice == "q":
+        break
+    else:
+        print('Input was invalid, please choose number from list')
+    print('Choice Registered')
+
+
 print('Done!')
